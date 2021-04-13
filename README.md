7a64c37df9d8c48ca83be9aa65b60135e52e0fc1
Biel Serrano <bielsesa@gmail.com>
2021-04-13 23:10:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## WooCommerce Utility
+
+A small project for an Electron app that lets you make API requests to WooCommerce for a specified Order ID<br>
+and then lets you visualize it's JSON data directly in the app.
+
+Technologies used: Electron, Tailwind CSS
---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+/**
+ * Creates the main window
+ */
+function createMainWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  win.loadFile('main.html');
+}
+app.whenReady().then(createMainWindow);
+// When all windows are closed exit the app, except in macOS.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+// When the application gets activated create the main window if one does not exist
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow()
+  }
+});
